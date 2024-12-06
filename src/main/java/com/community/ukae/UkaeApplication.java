bc568b6e6e8647cfd5f7ef2636d6e34080b05d36
algudgod <algudgod@gmail.com>
2024-12-06 22:48:11 +0900

    Initial commit

---
+++ src/main/java/com/community/ukae/UkaeApplication.java
@@ -0,0 +1,14 @@
+package com.community.ukae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkaeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("한글테스트");
+		SpringApplication.run(UkaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/community/ukae/UkaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.ukae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
