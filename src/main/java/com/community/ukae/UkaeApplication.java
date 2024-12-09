15dc47815c40363707d46cdf8f5a6adc6ceffc28
algudgod <algudgod@gmail.com>
2024-12-09 16:46:03 +0900

    feat: 사용자 기본 구조 및 홈 설정 추가
    - User 엔티티, 레포지토리, 서비스, DTO, 컨트롤러의 기본 뼈대 작성 완료
    - home.html 파일 생성: 간단한 기본 HTML 구조 추가
    - Thymeleaf 템플릿 엔진 설정 완료

--- src/main/java/com/community/ukae/UkaeApplication.java
+++ src/main/java/com/community/ukae/UkaeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UkaeApplication {
 
 	public static void main(String[] args) {
-		System.out.println("한글테스트");
 		SpringApplication.run(UkaeApplication.class, args);
 	}
 
