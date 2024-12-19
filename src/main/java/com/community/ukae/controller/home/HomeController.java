5ade341e77e4dc12ba7d21ac95280dcab1003163
algudgod <algudgod@gmail.com>
2024-12-19 15:09:58 +0900

    feat: Header, Footer 추가 및 홈 화면 게시판 구조 추가
        - 헤더와 푸터 템플릿 작성
        - home 페이지의 네비게이션을 헤더로 이동
        - home 로고 이미지를 헤더에 추가
        - 홈 화면에 게시판 구조 추가 (타이틀과 리스트)

--- src/main/java/com/community/ukae/controller/home/HomeController.java
+++ src/main/java/com/community/ukae/controller/home/HomeController.java
@@ -1,6 +1,11 @@
 package com.community.ukae.controller.home;
 
+import com.community.ukae.entity.user.User;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping
-    public String home() {
+    public String home(HttpSession session, Model model) {
+
+        User user = (User)session.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("user", user);
+            System.out.println("로그인된 사용자: " + user.getNickname());
+        } else {
+            System.out.println("세션에 사용자 정보가 없습니다.");
+        }
+
         return "home";
     }
 }
