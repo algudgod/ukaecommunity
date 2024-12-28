49934eafd6f2a4c86ca6e9777e1ba7de7280c8ab
algudgod <algudgod@gmail.com>
2024-12-28 17:39:05 +0900

    feat: 게시판 기능 기본 구성 추가
        - feature/board 브랜치 생성
        - Board 엔티티 및 레포지토리 구현
          - User 엔티티와 관계 매핑 설정
        - BoardCategory Enum 생성
          - 카테고리와 하위 카테고리 관리
          - 카테고리 이름과 하위 항목 리스트 포함
        - 게시판 관련 테이블 수정 및 구조 정의
          - subCategory 필드 추가

---
+++ src/main/java/com/community/ukae/entity/board/Board.java
@@ -0,0 +1,29 @@
+package com.community.ukae.entity.board;
+
+import com.community.ukae.entity.user.User;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int boardNo;
+    private String category;
+    private String subCategory;
+    private String tag;
+    private String title;
+    private String content;
+    @Column(updatable = false, insertable = false)
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+    @ManyToOne
+    @JoinColumn(name = "login_id", nullable = false)
+    private User user;
+    private int viewCount; // default: 0
+
+}
