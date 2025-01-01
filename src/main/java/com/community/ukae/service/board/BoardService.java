1f5931317c974e14cde1db48c24a9aec9f8f662c
algudgod <algudgod@gmail.com>
2025-01-01 15:04:03 +0900

    feat: 글쓰기 폼에서 제목과 내용 유효성 검증 및 에러 메시지 출력 구현
        - 제목: 3자 이상, 100자 이하로 제한
        - 내용: 최소 10자 이상, 2000자 이하로 제한
        - 유효성 검증 실패 시, 해당 에러 메시지를 폼에 표시

--- src/main/java/com/community/ukae/service/board/BoardService.java
+++ src/main/java/com/community/ukae/service/board/BoardService.java
@@ -5,7 +5,6 @@ import com.community.ukae.entity.board.Board;
 import com.community.ukae.entity.user.User;
 import com.community.ukae.enums.BoardCategory;
 import com.community.ukae.repository.board.BoardRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +20,7 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     public List<BoardRequestDTO> boardList(String mainCategory, String subCategory) {
-        // 레포지토리에서 게시글 목록 가져오기
+
         List<Board> boards = boardRepository.findByMainCategoryAndSubCategory(mainCategory, subCategory);
 
         // Board 엔티티를 BoardRequestDTO로 변환
@@ -32,7 +31,7 @@ public class BoardService {
     // 엔티티를 DTO로 변환하는 메서드
     private BoardRequestDTO convertToDTO(Board board) {
         return new BoardRequestDTO(
-                board.getBoardNo(),  // boardNo를 포함
+                board.getBoardNo(),
                 board.getMainCategory(),
                 board.getSubCategory(),
                 board.getTitle(),
@@ -44,7 +43,7 @@ public class BoardService {
         );
     }
 
-    // mainCategory와 subCategory에 해당하는 BoardCategory(Enum 상수)를 반환
+    // mainCategory와 subCategory에 해당하는 BoardCategory(Enum 상수)를 반환 (특정조건)
     public BoardCategory findBoardCategory(String mainCategory, String subCategory) {
         return Arrays.stream(BoardCategory.values())
                 .filter(category -> category.getMainCategory().equals(mainCategory) &&
@@ -62,6 +61,13 @@ public class BoardService {
     }
 
     public void addBoard(BoardRequestDTO boardRequest, User user) {
+
+        if (boardRequest.getTitle().length() < 3 || boardRequest.getTitle().length() > 100) {
+            throw new IllegalArgumentException("제목은 3자 이상, 100자 이하이어야 합니다.");
+        }
+        if (boardRequest.getContent().length() < 10 || boardRequest.getContent().length() > 2000) {
+            throw new IllegalArgumentException("내용은 최소 10자 이상, 2000자 이하이어야 합니다.");
+        }
 
         Board board = new Board();
         board.setMainCategory(boardRequest.getMainCategory());
